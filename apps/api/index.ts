7fdcd053a5e92073b33a2e5742e22c41cda50bde
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-05-31 22:09:04 +0530

    adding tests

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,15 +1,34 @@
 import express from "express";
+import prisma from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
 
 app.get("/website", (req, res) => {
   
+});
+
+app.post("/website",async (req, res) => {
+  const { name, url } = req.body; 
+  if(!url || !name) {
+  res.status(400).json({ error: "Name and url are required" });
+  return;
+  }  
+
+  const website = await prisma.website.create({
+    data: { 
+        name,
+        url,
+        timeAdded: new Date(),
+     },
+  });
+  res.json(website);
 });
 
 app.get("/website/:websiteId", (req, res) => {
 
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Server is running on port 3000");
 });
