2ea08841a089e3635ff5857c97bf9809b4d135c5
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-05-31 21:32:59 +0530

    api init

---
+++ apps/api/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/website", (req, res) => {
+  
+});
+
+app.get("/website/:websiteId", (req, res) => {
+
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
